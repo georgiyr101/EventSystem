8ef86473c96d87bf40fdc382d20b020466b22869
User <gromancuk871@gmail.com>
2026-04-05 14:20:57 +0300

    commit

--- src/test/java/com/example/eventsystem/EventSystemApplicationTests.java
+++ src/test/java/com/example/eventsystem/EventSystemApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.eventsystem;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class EventSystemApplicationTests {
 
